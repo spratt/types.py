3cbb22fc204e4b17d6d2e75e3415ccb19cd1676b
Simon Pratt <simon@pr4tt.com>
2014-01-02 16:21:54 -0500

    Added empty type inferer

---
+++ types.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python2.7
+'''
+types.py
+Copyright 2013-2014 Simon David Pratt
+See LICENSE for license information
+
+This program adds type inference to an AST in JSON format
+'''
+
+class Types(object):
+    def __init__(self):
+        pass
+
+def main():
+    import os
+
+if __name__ == '__main__':
+    main()
